6626c6013ab8ce5157c5108716a5c7c21084a49c
sevetom <sevetom2611@libero.it>
2024-07-09 13:05:49 +0200

    working on exporting parity

--- lib/src/logic/downloader/file_downloader_mobile.dart
+++ lib/src/logic/downloader/file_downloader_mobile.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
-import 'package:share_plus/share_plus.dart';
 import 'package:archive/archive.dart';
 import 'package:archive/archive_io.dart';
 
@@ -9,51 +8,38 @@ class FileDownloaderMobile {
   /// Save the deck file on mobile device.
   static Future<void> saveFileOnDevice(
       String fileName, String inFile, Map<String, String> mediaMap) async {
-    List bytes = [];
-    switch (Platform.operatingSystem) {
-      case 'android':
-        final directory = Directory("/storage/emulated/0/Download");
-        if (!directory.existsSync()) {
-          await directory.create();
+    if (mediaMap.isEmpty) {
+      final directory = Platform.isAndroid
+          ? Directory("/storage/emulated/0/Download")
+          : await getApplicationDocumentsDirectory();
+      final path = '${directory.path}/$fileName';
+      final file = File(path);
+      await file.writeAsString(inFile, flush: true);
+    } else {
+      List bytes = [];
+      final directory = await getTemporaryDirectory();
+      final path = '${directory.path}/$fileName';
+      final file = File(path);
+      final res = await file.writeAsString(inFile, flush: true);
+      bytes.add(await res.readAsBytes());
+      for (var i = 0; i < mediaMap.length; i++) {
+        bytes.add(await File(mediaMap.entries.elementAt(i).key).readAsBytes());
+      }
+      final archive = Archive();
+      for (var i = 0; i < bytes.length; i++) {
+        if (i == 0) {
+          archive.addFile(ArchiveFile(fileName, bytes[i].length, bytes[i]));
+        } else {
+          archive.addFile(ArchiveFile(mediaMap.entries.elementAt(i - 1).value,
+              bytes[i].length, bytes[i]));
         }
-        final path = '${directory.path}/$fileName';
-        final outFile = File(path);
-        await outFile.writeAsString(inFile, flush: true);
-        final res = await outFile.writeAsString(inFile, flush: true);
-        bytes.add(await res.readAsBytes());
-        if (mediaMap.isNotEmpty) {
-          for (var i = 0; i < mediaMap.length; i++) {
-            bytes.add(
-                await File(mediaMap.entries.elementAt(i).key).readAsBytes());
-          }
-          final archive = Archive();
-          for (var i = 0; i < bytes.length; i++) {
-            if (i == 0) {
-              archive.addFile(ArchiveFile(fileName, bytes[i].length, bytes[i]));
-            } else {
-              archive.addFile(ArchiveFile(
-                  mediaMap.entries.elementAt(i - 1).value,
-                  bytes[i].length,
-                  bytes[i]));
-            }
-          }
-          final zipEncoder = ZipEncoder();
-          final encodedFile = zipEncoder.encode(archive);
-          if (encodedFile != null) {
-            await File('${directory.path}/${fileName.split('.xml')[0]}.zip')
-                .writeAsBytes(encodedFile);
-          }
-        }
-        break;
-      case 'ios':
-        final directory = await getApplicationDocumentsDirectory();
-        final path = '${directory.path}/$fileName';
-        final file = File(path);
-        await file.writeAsString(inFile);
-        await Share.shareXFiles([XFile(path)]);
-        break;
-      default:
-        break;
+      }
+      final zipEncoder = ZipEncoder();
+      final encodedFile = zipEncoder.encode(archive);
+      if (encodedFile != null) {
+        await File('${directory.path}/${fileName.split('.xml')[0]}.zip')
+            .writeAsBytes(encodedFile);
+      }
     }
   }
 }
