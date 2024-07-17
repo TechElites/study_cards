ce7d3fb44707f1b8f8c3c645067a7f98b320db34
sevetom <sevetom2611@libero.it>
2024-07-17 16:38:59 +0200

    fixed rating buttons size

--- lib/src/composables/rating_buttons.dart
+++ lib/src/composables/rating_buttons.dart
@@ -7,27 +7,34 @@ class RatingButtons {
   /// Builds a list of rating buttons.
   static Widget build(BuildContext cx, Function(String rating) onPressed,
       {String selected = ''}) {
-    return ButtonBar(
-      alignment: MainAxisAlignment.spaceEvenly,
-      children: [
-      for (var rating
-          in Rating.colors.entries.where((e) => e.key != Rating.none))
-        ElevatedButton(
-          onPressed: () {
-            onPressed(rating.key);
-          },
-          style: ButtonStyle(
-            fixedSize: WidgetStateProperty.all(Size((MediaQuery.of(cx).size.width / 4.5), 50)),
-            foregroundColor: rating.key == selected
-                ? WidgetStateProperty.all(Colors.white)
-                : WidgetStateProperty.all(Colors.black),
-            backgroundColor: WidgetStateProperty.all(rating.value),
-            elevation: rating.key == selected
-                ? WidgetStateProperty.all(5)
-                : WidgetStateProperty.all(0),
-          ),
-          child: Text(rating.key.tr(cx)),
-        )
-    ]);
+    return FittedBox(
+        fit: BoxFit.scaleDown,
+        child: ButtonBar(alignment: MainAxisAlignment.spaceEvenly, children: [
+          for (var rating
+              in Rating.colors.entries.where((e) => e.key != Rating.none))
+            ElevatedButton(
+              onPressed: () {
+                onPressed(rating.key);
+              },
+              style: ButtonStyle(
+                fixedSize: WidgetStateProperty.all(
+                    Size((MediaQuery.of(cx).size.width / 4.5), 50)),
+                foregroundColor: rating.key == selected
+                    ? WidgetStateProperty.all(Colors.white)
+                    : WidgetStateProperty.all(Colors.black),
+                backgroundColor: WidgetStateProperty.all(rating.value),
+                elevation: rating.key == selected
+                    ? WidgetStateProperty.all(5)
+                    : WidgetStateProperty.all(0),
+              ),
+              child: FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(rating.key.tr(cx),
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ))),
+            )
+        ]));
   }
 }
