6d9d3ad63c59b4b448e58a6d29d00d2d91a0114e
sevetom <sevetom2611@libero.it>
2024-10-17 11:28:47 +0200

    moved to a composable the sidemenu

--- lib/src/screens/home/decks_page.dart
+++ lib/src/screens/home/decks_page.dart
@@ -1,10 +1,10 @@
 import 'dart:io';
+import 'package:flash_cards/src/composables/home_drawer.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 
 /// Import for mobile ads
 import 'package:flash_cards/src/composables/ads/ads_fullscreen.dart';
 import 'package:flash_cards/src/composables/ads/ads_sandman.dart';
-import 'package:flash_cards/src/data/repositories/reward_service.dart';
 
 import 'package:flash_cards/src/composables/ads/ads_scaffold.dart';
 import 'package:flash_cards/src/composables/floating_bar.dart';
@@ -12,18 +12,12 @@ import 'package:flash_cards/src/data/database/db_helper.dart';
 import 'package:flash_cards/src/logic/language/string_extension.dart';
 import 'package:flash_cards/src/logic/list_deleter.dart';
 import 'package:flash_cards/src/logic/permission_helper.dart';
-import 'package:flash_cards/src/screens/feedback/feedback.dart';
-import 'package:flash_cards/src/screens/guide/guide.dart';
-import 'package:flash_cards/theme/theme_provider.dart';
 import 'package:flash_cards/src/screens/add/add_deck.dart';
 import 'package:flash_cards/src/screens/home/cards_page.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:provider/provider.dart';
 import 'package:path/path.dart' as path;
-import 'package:url_launcher/url_launcher.dart';
 import 'package:vibration/vibration.dart';
-import 'package:http/http.dart' as http;
 
 /// Creates a page to display the list of all the decks
 class DecksPage extends StatefulWidget {
@@ -61,106 +55,7 @@ class _DecksPageState extends State<DecksPage> {
         title: Text('decks'.tr(cx)),
         centerTitle: true,
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Theme.of(cx).colorScheme.primary,
-                    Theme.of(cx).colorScheme.secondary
-                  ],
-                  transform: const GradientRotation(0.5),
-                ),
-              ),
-              padding: const EdgeInsets.only(top: 50),
-              child: const Text('Study Cards',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  textAlign: TextAlign.center),
-            ),
-            if (!kIsWeb)
-              ListTile(
-                title: Text('remove_ads'.tr(cx)),
-                leading: const Icon(Icons.tv_off),
-                onTap: () {
-                  Navigator.pop(cx);
-                  _adsSandman.showAndReloadAd(() {
-                    RewardService().setRewarded(true).then((_) {
-                      FloatingBar.show('ad_rewarded'.tr(cx), cx);
-                      setState(() {});
-                    });
-                  }).then((showed) {
-                    if (!showed) {
-                      FloatingBar.show('no_ads_left'.tr(cx), cx);
-                    }
-                  });
-                },
-              ),
-            ListTile(
-              title: Text('toggle_theme'.tr(cx)),
-              leading: Icon(
-                Provider.of<ThemeProvider>(cx).currentTheme == ThemeMode.light
-                    ? Icons.light_mode
-                    : (Provider.of<ThemeProvider>(cx).currentTheme ==
-                            ThemeMode.dark
-                        ? Icons.dark_mode
-                        : Icons.phone_android),
-              ),
-              onTap: () {
-                Provider.of<ThemeProvider>(cx, listen: false).toggleTheme();
-              },
-            ),
-            ListTile(
-              title: Text('send_feedback'.tr(cx)),
-              leading: const Icon(Icons.feedback),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const FeedbackPage(),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('guide'.tr(cx)),
-              leading: const Icon(Icons.menu_book_rounded),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const GuidePage(),
-                  ),
-                );
-              },
-            ),
-            if (kIsWeb)
-              ListTile(
-                  title: Text('download_apk'.tr(cx)),
-                  leading: const Icon(Icons.android),
-                  onTap: () => {
-                        _countDownload('android'),
-                        launchUrl(Uri.parse(
-                            "https://studycards.altervista.org/studycards.apk"))
-                      }),
-            if (kIsWeb)
-              ListTile(
-                title: Text('download_ipa'.tr(cx)),
-                leading: const Icon(Icons.apple),
-                onTap: () => {
-                  _countDownload('ios'),
-                  launchUrl(Uri.parse(
-                      "https://studycards.altervista.org/studycards.ipa"))
-                },
-              )
-          ],
-        ),
-      ),
+      drawer: HomeDrawer.build(cx, kIsWeb, _adsSandman),
       body: RefreshIndicator(
           onRefresh: () async {
             setState(() {});
@@ -322,12 +217,5 @@ class _DecksPageState extends State<DecksPage> {
         folder.deleteSync(recursive: true);
       }
     }
-  }
-
-  Future<void> _countDownload(String os) async {
-    http.post(
-      Uri.parse('http://studycards.altervista.org/count_download.php'),
-      body: {'content': os},
-    );
   }
 }
