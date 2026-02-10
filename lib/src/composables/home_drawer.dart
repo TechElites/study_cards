3c21a4ac778732f493e029c23a7b8a8368df88d4
sevetom <sevetom2611@libero.it>
2026-02-10 17:15:23 +0100

    feat: minor changes and removed older site api

--- lib/src/composables/home_drawer.dart
+++ lib/src/composables/home_drawer.dart
@@ -1,10 +1,8 @@
 import 'package:study_cards/src/logic/language/string_extension.dart';
-import 'package:study_cards/src/screens/feedback/feedback.dart';
 import 'package:study_cards/src/screens/guide/guide.dart';
 import 'package:study_cards/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 
 class HomeDrawer {
@@ -55,18 +53,6 @@ class HomeDrawer {
           },
         ),
         ListTile(
-          title: Text('send_feedback'.tr(cx)),
-          leading: const Icon(Icons.feedback),
-          onTap: () {
-            Navigator.push(
-              cx,
-              MaterialPageRoute(
-                builder: (cx) => const FeedbackPage(),
-              ),
-            );
-          },
-        ),
-        ListTile(
           title: Text('guide'.tr(cx)),
           leading: const Icon(Icons.menu_book_rounded),
           onTap: () {
@@ -78,43 +64,15 @@ class HomeDrawer {
             );
           },
         ),
-        if (!showAds)
-          ListTile(
-              title: Text('download_apk'.tr(cx)),
-              leading: const Icon(Icons.android),
-              onTap: () => {
-                    _countDownload('android'),
-                    launchUrl(Uri.parse(
-                        "https://studycards.altervista.org/studycards.apk"))
-                  }),
-        if (!showAds)
-          ListTile(
-            title: Text('download_ipa'.tr(cx)),
-            leading: const Icon(Icons.apple),
-            onTap: () => {
-              _countDownload('ios'),
-              launchUrl(
-                  Uri.parse("https://studycards.altervista.org/studycards.ipa"))
-            },
-          ),
-        if (!showAds)
-          ListTile(
-            title: Text('download_windows'.tr(cx)),
-            leading: const Icon(Icons.window),
-            onTap: () => {
-              _countDownload('windows'),
-              launchUrl(
-                  Uri.parse("https://studycards.altervista.org/studycards.zip"))
-            },
-          )
+        ListTile(
+          title: Text('download_versions'.tr(cx)),
+          leading: const Icon(Icons.download),
+          onTap: () => {
+            launchUrl(
+                Uri.parse("https://github.com/TechElites/study_cards/releases"))
+          },
+        )
       ],
     ));
-  }
-
-  static Future<void> _countDownload(String os) async {
-    http.post(
-      Uri.parse('http://studycards.altervista.org/count_download.php'),
-      body: {'content': os},
-    );
   }
 }
